0692b21fc816128fd7abceb8e51ae120d0b6361d
Josephine Aguilar <josieamor@Josephines-MacBook-Pro.local>
2018-10-12 21:48:58 -0600

    yikes

---
+++ create-react-express/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+
+app.use(routes);
+
+
+
+const db = require('./models')
+// Define API routes here
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact",
+{useNewUrlParser:true});
+
+
+
+
+
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function () {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
